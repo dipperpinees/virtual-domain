ba4f458be04f32f98901203b610ba6ada4a57d0f
Hiep Nguyen <hiepnk@bsscommerce.com>
2023-07-06 08:52:46 +0700

    fix sudo homedir for linux

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,6 +1,8 @@
 import path from "path";
 import os from "os";
 
-export const CONFIGS_PATH = path.join(os.homedir(), "./.bss-configs.json");
-export const CERTS_DIR_PATH = path.join(os.homedir(), "./.bss-certs");
-export const LOGS_PATH = path.join(os.homedir(), "./bss-logs.txt");
+const HOMEDIR = (process.platform === "linux" && process.env.SUDO_USER) ? `/home/${process.env.SUDO_USER}` : os.homedir();
+
+export const CONFIGS_PATH = path.join(HOMEDIR, "./.bss-configs.json");
+export const CERTS_DIR_PATH = path.join(HOMEDIR, "./.bss-certs");
+export const LOGS_PATH = path.join(HOMEDIR, "./bss-logs.txt");
